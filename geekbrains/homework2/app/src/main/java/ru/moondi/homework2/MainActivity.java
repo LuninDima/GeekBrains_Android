d879c26a73679d62660d93451cfb1c2fae2189a7
LuninDima <moon-di-2009@yandex.ru>
2021-04-24 18:32:43 +0300

    Добавлены элементы кнопок калькулятора в макет activity_main.xml

--- geekbrains/homework2/app/src/main/java/ru/moondi/homework2/MainActivity.java
+++ geekbrains/homework2/app/src/main/java/ru/moondi/homework2/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+// Для приложения будет использоваться ConstraintLayout как наиболее универсальный и гибкий.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
