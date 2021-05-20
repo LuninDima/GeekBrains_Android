e11e338165dce9539fbf295ca564ec3085e63bd9
LuninDima <moon-di-2009@yandex.ru>
2021-05-20 21:28:08 +0300

    создание слушателя нажатия для ввода чисел кнопками в поле textDisplay

--- geekbrains/homework2/app/src/main/java/ru/moondi/homework2/MainActivity.java
+++ geekbrains/homework2/app/src/main/java/ru/moondi/homework2/MainActivity.java
@@ -4,13 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-// Для приложения будет использоваться ConstraintLayout как наиболее универсальный и гибкий.
+    TextView text;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.textDisplay);
+
+    }
+    public void intoNumber(View view){
+        Button b = (Button) view;
+        text.setText(text.getText().toString() + b.getText().toString());
 
     }
 }
