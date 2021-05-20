158fa95646d32d3bd8404369bf1593e0786b707d
LuninDima <moon-di-2009@yandex.ru>
2021-05-20 21:56:19 +0300

    добавление логики калькулятора

--- geekbrains/homework2/app/src/main/java/ru/moondi/homework2/MainActivity.java
+++ geekbrains/homework2/app/src/main/java/ru/moondi/homework2/MainActivity.java
@@ -10,16 +10,46 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView text;
+    int buffer;
+    char operation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        buffer = 0;
+        operation = '1';
         text = findViewById(R.id.textDisplay);
-
+        Button equal = findViewById(R.id.button_equally);
+        equal.setOnClickListener(View -> {
+        switch (operation){
+            case '+':
+                buffer += Integer.parseInt(text.getText().toString());
+                text.setText(String.valueOf(buffer));
+                break;
+            case '-':
+                buffer -= Integer.parseInt(text.getText().toString());
+                text.setText(String.valueOf(buffer));
+                break;
+            case '/':
+                buffer /= Integer.parseInt(text.getText().toString());
+                text.setText(String.valueOf(buffer));
+                break;
+            case '*':
+                buffer *= Integer.parseInt(text.getText().toString());
+                text.setText(String.valueOf(buffer));
+                break;
+        }
+        });
     }
     public void intoNumber(View view){
         Button b = (Button) view;
         text.setText(text.getText().toString() + b.getText().toString());
 
+    }
+    public void calculate(View view){
+        Button b = (Button) view;
+    buffer = Integer.parseInt(text.getText().toString());
+    operation = b.getText().charAt(0);
+    text.setText("");
     }
 }
